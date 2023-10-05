b402b4d8cae80e3649f6623a7a480223b4c550b1
Angelique Priam <angelique.priam@hotmail.com>
2023-10-05 13:10:09 +0200

    refacto auth handler

--- src/app/components/auth/AuthForm.tsx
+++ src/app/components/auth/AuthForm.tsx
@@ -11,12 +11,18 @@ import {
 	AccountCircle,
 	Lock,
 } from "@mui/icons-material";
-import {
-	handleSubmitLogin,
-	handleSubmitSignUp,
-} from "../../../utils/handlers/auth.handlers";
 import Link from "@mui/material/Link";
-import { usePathname } from "next/navigation";
+import {
+	usePathname,
+	useRouter,
+} from "next/navigation";
+import {
+	AccessTokenDto,
+	ApiService,
+	LoginDto,
+	RegisterDto,
+	UserGetDto,
+} from "../../../services/api.service";
 
 export default function AuthForm() {
 	const currentUrl = usePathname();
@@ -28,7 +34,68 @@ export default function AuthForm() {
 		React.useState("");
 	const [password, setPassword] =
 		React.useState("");
+	const router = useRouter();
+	const apiService: ApiService =
+		new ApiService(
+			"http://localhost:3003"
+		);
 
+	const handleSubmitLogin = async (
+		email: string,
+		password: string
+	) => {
+		try {
+			const result: AccessTokenDto =
+				await apiService.authSignIn(
+					{
+						email,
+						password,
+					} as LoginDto
+				);
+			if (result.accessToken) {
+				alert(
+					"Connexion réussie" +
+						email
+				);
+				localStorage.setItem(
+					"access_token",
+					result.accessToken
+				);
+				router.push("/profile");
+			}
+		} catch (error) {
+			alert(
+				"Erreur lors de la connexion : " +
+					error
+			);
+		}
+	};
+
+	const handleSubmitSignUp = async (
+		email: string,
+		password: string
+	) => {
+		try {
+			const result: UserGetDto =
+				await apiService.authSignUp(
+					{
+						email,
+						password,
+					} as RegisterDto
+				);
+			console.log(result);
+			alert(
+				"Inscription réussie " +
+					email
+			);
+			router.push("/login");
+		} catch (error) {
+			alert(
+				"Erreur lors de l'inscription : " +
+					error
+			);
+		}
+	};
 	return (
 		<Container maxWidth="xs">
 			<form
